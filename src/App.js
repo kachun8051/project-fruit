93e03e22874b711a88ee3fb88420ca824516b410
kachun <kcwong8051@gmail.com>
2023-08-05 17:09:55 +0800

    change the datasource to back4app

--- src/App.js
+++ src/App.js
@@ -1,19 +1,56 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Parse from 'parse/dist/parse.min.js';
+import { PARSE_APPLICATION_ID, PARSE_HOST_URL, PARSE_JAVASCRIPT_KEY } from './data/keys';
 import HomePage from './pages/HomePage';
 import FruitList from './pages/FruitList';
 import FruitDetail from './pages/FruitDetail';
 import JuiceList from './pages/JuiceList';
 import Branch from './pages/Branch';
-import fruitJson from './data/fruitlist.json';
 import './App.css';
 
 export const Context = React.createContext();
+// Your Parse initialization configuration goes here
+Parse.initialize(PARSE_APPLICATION_ID, PARSE_JAVASCRIPT_KEY);
+Parse.serverURL = PARSE_HOST_URL;
 
 function App() {
-  //fruits
-  const fruits = fruitJson;
-  const [ fruit, setfruit ] = useState(fruits);
+  
+  const [ fruit, setfruit ] = useState([]);  
+
+  const fetchAllFruits = async() => {
+    const query = new Parse.Query('fruitlist');
+    // find all rows of fruitlist
+    const lstQueried = await query.find();
+    // first, make the object into json string
+    let jsonStr = JSON.stringify(lstQueried);
+    console.log(jsonStr);
+    // second, convert the json string into object
+    let lstFruit = JSON.parse(jsonStr);
+    let lst = [];
+    for (let i=0; i<lstFruit.length; i++) {
+      let obj = {
+        'id': lstFruit[i].seq,
+        'name': lstFruit[i].name,
+        'price': lstFruit[i].price,
+        'image': lstFruit[i].image,
+        'description': lstFruit[i].description,
+        'url': lstFruit[i].url
+      }
+      lst.push(obj);
+    }
+    setfruit(lst);    
+  }
+
+  // fetch data one time when rendering
+  useEffect(
+    () => {
+       //fetch("https://hh68057a.github.io/demo/product_demo1.json")
+       //  .then((Response) => Response.json())
+       //  .then((data) => setfruit(data));            
+      fetchAllFruits();
+    }, []
+  );
 
   return (
     <Context.Provider value={[ fruit, setfruit ]}>
@@ -25,8 +62,8 @@ function App() {
         <Link className="box" to="/branch">Branch</Link>
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/fruit" element={<FruitList data={fruit} />} />
-          <Route path="/fruitdetail" element={<FruitDetail  datasrc={fruit} />}>
+          <Route path="/fruit" element={<FruitList />} />
+          <Route path="/fruitdetail" element={<FruitDetail />}>
             <Route path=":fid" element={<FruitDetail />} />
           </Route>
           <Route path="/juice" element={<JuiceList />} />
---
+++ src/data/keys.js
@@ -0,0 +1,6 @@
+// Your Parse initialization configuration goes here
+const PARSE_APPLICATION_ID = '5vUD5SzypdFDZfa7Sxjya1yLliHMAJ52ML3sqBf6';
+const PARSE_HOST_URL = 'https://parseapi.back4app.com/';
+const PARSE_JAVASCRIPT_KEY = 'pQCa8PbENykHJkGvzHBpZLlgVaI7phqgnh2vf0eB';
+
+export {PARSE_APPLICATION_ID, PARSE_HOST_URL, PARSE_JAVASCRIPT_KEY};
